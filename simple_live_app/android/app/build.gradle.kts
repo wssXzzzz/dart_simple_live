02261a566c13e49977e048e0e5c9ff86b4a99b26
全岛铁盒 <34977534+wssXzzzz@users.noreply.github.com>
2026-06-16 23:30:34 +0800

    删除正式签名配置块

--- simple_live_app/android/app/build.gradle.kts
+++ simple_live_app/android/app/build.gradle.kts
@@ -39,16 +39,6 @@ android {
         versionName = flutter.versionName
     }
 
-    signingConfigs {
-        create("release") {
-            keyAlias = keystoreProperties["keyAlias"] as String
-            keyPassword = keystoreProperties["keyPassword"] as String
-            storeFile = keystoreProperties["storeFile"]?.let { file(it) }
-            storePassword = keystoreProperties["storePassword"] as String
-            isV1SigningEnabled = true
-            isV2SigningEnabled = true
-        }
-    }
 
     buildTypes {
         release {
